71717dd28eaa6268949679e6ce4c8a6d6c38ffd4
kryyyaaaa <137829849+kryyyaaaa@users.noreply.github.com>
2023-07-15 14:54:45 +0300

    Add files via upload

---
+++ what.py
@@ -0,0 +1,144 @@
+import time
+import os
+import webbrowser
+
+## Контакты:
+## Дискорд ==> @kryyaa
+## Телеграм ==> @kryyyaaaa
+## ГитХаб ==> @kryyyaaaa
+
+os.system('CLS')
+
+print("*visual only")
+
+time.sleep(1)
+
+os.system('CLS')
+
+print("                            телеграм: @kryyyaaaa")
+print("-----------------------------")
+print("| ╔╗╔╗╔╦╗╔╦══╦════╗╔═══╦╗╔╗ |")
+print("| ║║║║║║║║║╔╗╠═╗╔═╝║╔═╗║║║║ |")
+print("| ║║║║║║╚╝║╚╝║─║║──║╚═╝║╚╝║ |")
+print("| ║║║║║║╔╗║╔╗║─║║──║╔══╩═╗║ |")
+print("| ║╚╝╚╝║║║║║║║─║║─╔╣║───╔╝║ |")
+print("| ╚═╝╚═╩╝╚╩╝╚╝─╚╝─╚╩╝───╚═╝ |")
+print("-----------------------------")
+print("")
+print("")
+print("")
+print("Ваша версия: .py")
+print("What.py - Лучший exploit для взлома!")
+print("")
+print("1. Взлом WiFi")
+print("2. Создать Петю (Petya.a)")
+print("3. Запустить бомбер")
+print("4. Взломать игру Steam")
+print("5. Взлом аккаунта Телеграм")
+print("6. Взлом аккаунта ВК")
+print("7. Взлом аккаунта WhatsApp")
+print("8. Взлом аккаунта Steam")
+
+what = int(input("Введите цифру: "))
+
+if what == 1:
+	os.system('CLS')
+
+	input("Введите название сети: ")
+	os.system('CLS')
+	print("Анализирую сеть...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Ищу дыры...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Использую WiFiTool...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Подбираю пароль...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Пароль: FpIYnuPaG1Hq6ZxV9")
+	input()
+
+elif what == 2:
+	os.system('CLS')
+
+	print("Премиум функция!")
+	input()
+
+elif what == 3:
+	os.system('CLS')
+
+	webbrowser.open('https://github.com/x11-repo/JamSpymer', new=2)
+
+elif what == 4:
+	os.system('CLS')
+
+	game = input("Введите игру: ")
+	os.system('CLS')
+	print("Ищу игру...")
+	time.sleep(3)
+	os.system('CLS')
+	print('Найдена игра:', game)
+	time.sleep(0.4)
+	print("Загружаю на сервер...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Готово!")
+	print("Чекни свой стим аккаунт")
+	input
+
+elif what == 5:
+	os.system('CLS')
+
+	accTG = input("Введите аккаунт(Пример: @AccountTG): ")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Изучаю базу данных...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Ищу утечки информации...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Готово!")
+	time.sleep(0.2)
+	print('Аккаунт: ', accTG)
+	print('Пароль: 0e6Z5P5CSCsjwY')
+	print("Облачный пароль: Nl1fpnVKpFM79Q")
+	input()
+
+elif what == 6:
+	os.system('CLS')
+
+	accVK = input("Введите аккаунт(Пример: @AccountVK): ")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Изучаю базу данных...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Ищу утечки информации...")
+	time.sleep(0.9)
+	os.system('CLS')
+	print("Готово!")
+	time.sleep(0.2)
+	print('Аккаунт: ', accVK)
+	print('Пароль: 6FDen0uMaIQAn')
+	print("Облачный пароль: nV9q7Cth9i2F5F")
+	input()
+
+elif what == 7:
+	os.system('CLS')
+
+	accWA = input("Введите номер телефона(Пример: +79999999999): ")
+	time.sleep(4)
+	print("К сожелению, What не работает с WhatsApp!")
+	input()
+
+elif what == 8:
+	print("Премиум функция!")
+	input()
+
+else:
+	print("Не правильно введено число!")
+	input()
